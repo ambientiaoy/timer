a588e45b682b0254bdbf92040709d794371b5288
Aki Salmi <aki.salmi@ambientia.fi>
2016-05-19 20:35:05 +0300

    emiting and receiving socket-emited time

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ var app = express();
 
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+
+var aika = 60;
+
 io.on("connection", function(socket) {
     console.log("Connection hello");
+    socket.on("reset", function(){
+        console.log("reset");
+        aika = 60;
+        setInterval(function( ) {
+            aika = aika - 1;
+            socket.emit("time", aika);
+        }, 1000);
+    });
 });
 
 //app.use(express.static("public"));
 
 app.get("/", function(request, response) {
-   response.sendFile(__dirname+"/public/index.html");
+    response.sendFile(__dirname+"/public/index.html");
 });
 
 app.get("/client.js", function(request, response) {
--- public/client.js
+++ public/client.js
@@ -2,4 +2,12 @@ $(document).ready( function(){
     console.log("client.js toimii");
 
     var socket = io();
+
+    $("#nappi").on("click", function(){
+        socket.emit("reset");
+    });
+
+    socket.on("time", function(time){
+        $("#aika").html(time);
+    });
 });
